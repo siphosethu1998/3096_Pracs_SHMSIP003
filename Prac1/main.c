b669239c11967ab04555098201b1d9f52736ff72
siphosethu1998 <siphosethu1998@gmail.com>
2022-08-01 17:53:15 +0200

    before debugging main

---
+++ Prac1/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h> 
+
+int main(){
+    int a,b, sum;
+
+    printf("Enter a value for a: ");
+    scanf("%d", a);
+
+
+    printf("Enter a value for b: ");
+    scanf("%d", a);
+
+    sum = a + b;
+
+    printf("The sum of a b and b is %d \n", sum);
+    return 0;
+}
